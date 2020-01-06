5e2807f40453fad2e681fb53b56be346c449e583
PickNose <48274192+AhnaGeneral@users.noreply.github.com>
2020-01-06 12:33:39 +0900

    (200106)kkongA Framework
    다중랜더타겟 프레임워크 입니다.

---
+++ Client/CorruptLab/CorruptLab/Shaders.hlsl
@@ -0,0 +1,235 @@
+cbuffer cbPlayerInfo : register(b0)
+{
+	matrix		gmtxPlayerWorld : packoffset(c0);
+};
+
+cbuffer cbCameraInfo : register(b1)
+{
+	matrix		gmtxView : packoffset(c0);
+	matrix		gmtxProjection : packoffset(c4);
+	float3		gvCameraPosition : packoffset(c8);
+};
+
+cbuffer cbGameObjectInfo : register(b2)
+{
+	matrix		gmtxGameObject : packoffset(c0);
+	uint		gnObjectID : packoffset(c4);
+	uint		gnMaterialID : packoffset(c8);
+};
+
+#include "Light.hlsl"
+
+struct VS_DIFFUSED_INPUT
+{
+	float3 position : POSITION;
+	float4 color : COLOR;
+};
+
+struct VS_DIFFUSED_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float4 color : COLOR;
+};
+
+VS_DIFFUSED_OUTPUT VSDiffused(VS_DIFFUSED_INPUT input)
+{
+	VS_DIFFUSED_OUTPUT output;
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+	output.color = input.color;
+
+	return(output);
+}
+
+float4 PSDiffused(VS_DIFFUSED_OUTPUT input) : SV_TARGET
+{
+	return(input.color);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+VS_DIFFUSED_OUTPUT VSPlayer(VS_DIFFUSED_INPUT input)
+{
+	VS_DIFFUSED_OUTPUT output;
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxPlayerWorld), gmtxView), gmtxProjection);
+	output.color = input.color;
+
+	return(output);
+}
+
+float4 PSPlayer(VS_DIFFUSED_OUTPUT input) : SV_TARGET
+{
+	return(input.color);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+Texture2DArray gtxtTextureArray : register(t0);
+SamplerState gSamplerState : register(s0);
+
+struct VS_TEXTURED_INPUT
+{
+	float3 position : POSITION;
+	float2 uv : TEXCOORD;
+};
+
+struct VS_TEXTURED_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD;
+};
+
+VS_TEXTURED_OUTPUT VSTextured(VS_TEXTURED_INPUT input)
+{
+	VS_TEXTURED_OUTPUT output;
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+	output.uv = input.uv;
+
+	return(output);
+}
+
+float4 PSTextured(VS_TEXTURED_OUTPUT input, uint nPrimitiveID : SV_PrimitiveID) : SV_TARGET
+{
+	float3 uvw = float3(input.uv, nPrimitiveID / 2);
+	float4 cColor = gtxtTextureArray.Sample(gSamplerState, uvw);
+
+	return(cColor);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+#define _WITH_VERTEX_LIGHTING
+
+struct VS_TEXTURED_LIGHTING_INPUT
+{
+	float3 position : POSITION;
+	float3 normal : NORMAL;
+	float2 uv : TEXCOORD;
+};
+
+struct VS_TEXTURED_LIGHTING_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float3 positionW : POSITION;
+	float3 normalW : NORMAL;
+	//	nointerpolation float3 normalW : NORMAL;
+	float2 uv : TEXCOORD;
+#ifdef _WITH_VERTEX_LIGHTING
+	float4 color : COLOR;
+#endif
+};
+
+VS_TEXTURED_LIGHTING_OUTPUT VSTexturedLighting(VS_TEXTURED_LIGHTING_INPUT input)
+{
+	VS_TEXTURED_LIGHTING_OUTPUT output;
+
+	output.normalW = mul(input.normal, (float3x3)gmtxGameObject);
+	output.positionW = (float3)mul(float4(input.position, 1.0f), gmtxGameObject);
+	output.position = mul(mul(float4(output.positionW, 1.0f), gmtxView), gmtxProjection);
+	output.uv = input.uv;
+#ifdef _WITH_VERTEX_LIGHTING
+	output.normalW = normalize(output.normalW);
+	output.color = Lighting(output.positionW, output.normalW);
+#endif
+	return(output);
+}
+
+float4 PSTexturedLighting(VS_TEXTURED_LIGHTING_OUTPUT input, uint nPrimitiveID : SV_PrimitiveID) : SV_TARGET
+{
+	float3 uvw = float3(input.uv, nPrimitiveID / 2);
+	float4 cColor = gtxtTextureArray.Sample(gSamplerState, uvw);
+#ifdef _WITH_VERTEX_LIGHTING
+	float4 cIllumination = input.color;
+#else
+	input.normalW = normalize(input.normalW);
+	float4 cIllumination = Lighting(input.positionW, input.normalW);
+#endif
+	return(cColor * cIllumination);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+struct PS_MULTIPLE_RENDER_TARGETS_OUTPUT
+{
+	float4 color : SV_TARGET0;
+	float4 normal : SV_TARGET1;
+	float4 object : SV_TARGET2;
+};
+
+PS_MULTIPLE_RENDER_TARGETS_OUTPUT PSTexturedLightingToMultipleRTs(VS_TEXTURED_LIGHTING_OUTPUT input, uint nPrimitiveID : SV_PrimitiveID)
+{
+	PS_MULTIPLE_RENDER_TARGETS_OUTPUT output;
+
+	float3 uvw = float3(input.uv, nPrimitiveID / 2);
+	float4 cColor = gtxtTextureArray.Sample(gSamplerState, uvw);
+
+#ifdef _WITH_VERTEX_LIGHTING
+	output.color = input.color * cColor;
+#else
+	input.normalW = normalize(input.normalW);
+	float4 cIllumination = Lighting(input.positionW, input.normalW);
+	output.color = cIllumination * cColor;
+#endif
+
+	output.normal.xyz = input.normalW.xyz * 0.5f + 0.5f;
+
+	output.object = float4(gnObjectID / 9261.0f, gnObjectID / 9261.0f, gnObjectID / 9261.0f, gnObjectID / 9261.0f);
+
+	return(output);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+float4 VSPostProcessing(uint nVertexID : SV_VertexID) : SV_POSITION
+{
+	if (nVertexID == 0) return(float4(-1.0f, +1.0f, 0.0f, 1.0f));
+	if (nVertexID == 1) return(float4(+1.0f, +1.0f, 0.0f, 1.0f));
+	if (nVertexID == 2) return(float4(+1.0f, -1.0f, 0.0f, 1.0f));
+	if (nVertexID == 3) return(float4(-1.0f, +1.0f, 0.0f, 1.0f));
+	if (nVertexID == 4) return(float4(+1.0f, -1.0f, 0.0f, 1.0f));
+	if (nVertexID == 5) return(float4(-1.0f, -1.0f, 0.0f, 1.0f));
+
+	return(float4(0, 0, 0, 0));
+}
+
+Texture2D<float4> gtxtScene : register(t1);
+
+float4 PSPostProcessing(float4 position : SV_POSITION) : SV_Target
+{
+	float3 cColor = gtxtScene[int2(position.xy)].rgb;
+
+	return(float4(cColor, 1.0f));
+}
+
+static float gfLaplacians[9] = { -1.0f, -1.0f, -1.0f, -1.0f, 8.0f, -1.0f, -1.0f, -1.0f, -1.0f };
+static int2 gnOffsets[9] = { { -1,-1 },{ 0,-1 },{ 1,-1 },{ -1,0 },{ 0,0 },{ 1,0 },{ -1,1 },{ 0,1 },{ 1,1 } };
+//static int2 gnOffsets[9] = { {-1,-1}, {0,-1}, int2(1,-1), int2(-1,0), int2(0,0), int2(1,0), int2(-1,1), int2(0,1), int2(1,1) };
+
+Texture2D<float4> gtxtNormal : register(t2);
+Texture2D<float4> gtxtObject : register(t3);
+
+float4 PSPostProcessingByLaplacianEdge(float4 position : SV_POSITION) : SV_Target
+{
+	float fEdgeness = 0.0f;
+	float3 cEdgeness = float3(0.0f, 0.0f, 0.0f);
+	if ((uint(position.x) >= 1) || (uint(position.y) >= 1) || (uint(position.x) <= gtxtNormal.Length.x - 2) || (uint(position.y) <= gtxtNormal.Length.y - 2))
+	{
+		float fObject = 0.0f;
+		for (int i = 0; i < 9; i++)
+		{
+			float3 vNormal = gtxtNormal[int2(position.xy) + gnOffsets[i]].xyz;
+			vNormal = vNormal * 2.0f - 1.0f;
+			cEdgeness += gfLaplacians[i] * vNormal;
+			fObject += gtxtObject[int2(position.xy) + gnOffsets[i]].r * (1.0f / 9.0f);
+		}
+		fEdgeness = cEdgeness.r * 0.3f + cEdgeness.g * 0.59f + cEdgeness.b * 0.11f;
+		if ((fEdgeness < 0.15f) && (abs(fObject - gtxtObject[int2(position.xy)].r) > 0.01f)) fEdgeness = 1.0f;
+		cEdgeness = float3(fEdgeness, fEdgeness, fEdgeness);
+	}
+	float3 cColor = gtxtScene[int2(position.xy)].rgb;
+	cColor = (fEdgeness < 0.15f) ? cColor : ((fEdgeness < 0.65f) ? (cColor + cEdgeness) : cEdgeness);
+
+	return(float4(cColor, 1.0f));
+}
