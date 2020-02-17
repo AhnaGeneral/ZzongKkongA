ee8ec1ec4c609199dd4bc12f858448fe43084b53
PickNose <48274192+AhnaGeneral@users.noreply.github.com>
2020-02-18 03:11:46 +0900

    (200218)Ahna_Animation #3_2
    애니메이션 메쉬의 부모를 어떤 클래스로 해야하는지 많이 고민이 된다.

--- Client/CorruptLab/CorruptLab/Mesh.h
+++ Client/CorruptLab/CorruptLab/Mesh.h
@@ -106,7 +106,8 @@ public:
 	void ReleaseUploadBuffers();
 
 protected:
-	MeshInfo   m_MeshInfo;
+	MeshInfo              m_MeshInfo;
+	MeshLoadInfo          m_MeshLoadInfo;
 public:
 	UINT GetType() { return(m_MeshInfo.m_nType); }
 	virtual void Render(ID3D12GraphicsCommandList *pd3dCommandList);
@@ -138,39 +139,39 @@ public:
 
 #define SKINNED_ANIMATION_BONES		128
 
-class CSkinnedMesh : public CStandardMesh
+class CSkinnedMesh : public CMesh
 {
 public:
 	CSkinnedMesh(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
 	virtual ~CSkinnedMesh();
 
 protected:
-	int								m_nBonesPerVertex = 4;
-
-	XMINT4* m_pxmn4BoneIndices = NULL;
-	XMFLOAT4* m_pxmf4BoneWeights = NULL;
-
-	ID3D12Resource* m_pd3dBoneIndexBuffer = NULL;
-	ID3D12Resource* m_pd3dBoneIndexUploadBuffer = NULL;
+	int				                m_nBonesPerVertex = 4;
+					              
+	XMINT4                        * m_pxmn4BoneIndices = NULL;
+	XMFLOAT4                      * m_pxmf4BoneWeights = NULL;
+					              
+	ID3D12Resource                * m_pd3dBoneIndexBuffer = NULL;
+	ID3D12Resource                * m_pd3dBoneIndexUploadBuffer = NULL;
 	D3D12_VERTEX_BUFFER_VIEW		m_d3dBoneIndexBufferView;
 
-	ID3D12Resource* m_pd3dBoneWeightBuffer = NULL;
-	ID3D12Resource* m_pd3dBoneWeightUploadBuffer = NULL;
+	ID3D12Resource                * m_pd3dBoneWeightBuffer = NULL;
+	ID3D12Resource                * m_pd3dBoneWeightUploadBuffer = NULL;
 	D3D12_VERTEX_BUFFER_VIEW		m_d3dBoneWeightBufferView;
 
 public:
 	int								m_nSkinningBones = 0;
 
-	char(*m_ppstrSkinningBoneNames)[64];
-	CGameObject** m_ppSkinningBoneFrameCaches = NULL; //[m_nSkinningBones]
+	char                          (*m_ppstrSkinningBoneNames)[64];
+	CGameObject                  ** m_ppSkinningBoneFrameCaches = NULL; //[m_nSkinningBones]
 
-	XMFLOAT4X4* m_pxmf4x4BindPoseBoneOffsets = NULL; //Transposed
+	XMFLOAT4X4                    * m_pxmf4x4BindPoseBoneOffsets = NULL; //Transposed
 
-	ID3D12Resource* m_pd3dcbBindPoseBoneOffsets = NULL;
-	XMFLOAT4X4* m_pcbxmf4x4MappedBindPoseBoneOffsets = NULL;
+	ID3D12Resource                * m_pd3dcbBindPoseBoneOffsets = NULL;
+	XMFLOAT4X4                    * m_pcbxmf4x4MappedBindPoseBoneOffsets = NULL;
 
-	ID3D12Resource* m_pd3dcbSkinningBoneTransforms = NULL;
-	XMFLOAT4X4* m_pcbxmf4x4MappedSkinningBoneTransforms = NULL;
+	ID3D12Resource                * m_pd3dcbSkinningBoneTransforms = NULL;
+	XMFLOAT4X4                    * m_pcbxmf4x4MappedSkinningBoneTransforms = NULL;
 
 public:
 	void PrepareSkinning(CGameObject* pModelRootObject);
