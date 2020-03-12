00f64138867cb91db68b2e539c9d37028815fd41
dahyun-gwon <43131696+dahyun-gwon@users.noreply.github.com>
2020-03-13 04:17:26 +0900

    (200313)Jjong_Changed Texture

---
+++ Test_In_Unity/tmp/Assets/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float fMoveSpeed = 10.0f;
+    public float fRotateSpeed = 50.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * fMoveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.back * fMoveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up * fRotateSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up * -fRotateSpeed * Time.deltaTime);
+    }
+}
